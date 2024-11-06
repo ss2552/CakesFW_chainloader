08fe74a8a4901785ed5d9c418158196b5849f401
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-11-07 01:41:56 +0900

    Update firm.c

--- source/firm.c
+++ source/firm.c
@@ -23,6 +23,7 @@
 #include <string.h>
 #endif
 
+#include "types.h"
 #ifndef STANDALONE
 firm_h *firm_orig_loc = (firm_h *)FCRAM_FIRM_ORIG_LOC;
 size_t firm_size = FCRAM_SPACING;
@@ -521,4 +522,30 @@ void launchFirm(int argc, char **argv)
 {
     prepareArm11ForFirmlaunch();
     chainload(argc, argv, firm);
+}
+void loadHomebrewFirm(uint32_t pressed)
+{
+    char path[10 + 255];
+    bool hasDisplayedMenu = false;
+    bool found = !pressed ? payloadMenu(path, &hasDisplayedMenu) : findPayload(path, pressed);
+
+    if(!found) return;
+
+    u32 maxPayloadSize = (u32)((u8 *)0x27FFE000 - (u8 *)firm),
+        payloadSize = fileRead(firm, path, maxPayloadSize);
+
+    if(payloadSize <= 0x200 || !checkFirm(payloadSize)) error("The payload is invalid or corrupted.");
+
+    char absPath[24 + 255];
+
+    if(isSdMode) sprintf(absPath, "sdmc:/luma/%s", path);
+    else sprintf(absPath, "nand:/rw/luma/%s", path);
+
+    char *argv[2] = {absPath, (char *)fbs};
+    bool wantsScreenInit = (firm->reserved2[0] & 1) != 0;
+
+    if(!hasDisplayedMenu && wantsScreenInit)
+        initScreens(); // Don't init the screens unless we have to, if not already done
+
+    launchFirm(wantsScreenInit ? 2 : 1, argv);
 }
