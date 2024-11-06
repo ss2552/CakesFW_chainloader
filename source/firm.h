24ef2d792c356a75e1d1ad598ee50d4e65280928
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-11-07 00:23:20 +0900

    Update firm.h

--- source/firm.h
+++ source/firm.h
@@ -6,6 +6,8 @@
 #include "headers.h"
 #include "firm_signatures.h"
 
+#include "3dsheaders.h"
+
 extern firm_h *firm_orig_loc;
 extern size_t firm_size;
 extern struct firm_signature *current_firm;
@@ -22,3 +24,11 @@ void slot0x11key96_init();
 int load_firms();
 void boot_firm();
 void boot_cfw();
+
+u32 loadNintendoFirm(FirmwareType *firmType, FirmwareSource nandType, bool loadFromStorage, bool isSafeMode);
+void loadHomebrewFirm(u32 pressed);
+u32 patchNativeFirm(u32 firmVersion, FirmwareSource nandType, bool loadFromStorage, bool isFirmProtEnabled, bool needToInitSd, bool doUnitinfoPatch);
+u32 patchTwlFirm(u32 firmVersion, bool loadFromStorage, bool doUnitinfoPatch);
+u32 patchAgbFirm(bool loadFromStorage, bool doUnitinfoPatch);
+u32 patch1x2xNativeAndSafeFirm(void);
+void launchFirm(int argc, char **argv);
